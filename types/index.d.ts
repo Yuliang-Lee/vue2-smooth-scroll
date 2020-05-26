ce67f36597b4f56d61c92d20e8136a6efddf395a
James Whitmarsh <jwhitmarsh@dresources.com>
2020-05-26 13:34:55 +0100

    build(types): add TypeScript types

---
+++ types/index.d.ts
@@ -0,0 +1,17 @@
+import _Vue, { PluginFunction } from "vue";
+
+// augment typings of Vue.js
+import "./vue-injections";
+
+export interface SmoothScrollOptions {
+  scrollTo: Element;
+  duration?: number;
+  offset?: number;
+  container?: Element;
+  updateHistory?: boolean;
+  hash?: string; // required if updateHistory is true
+}
+
+declare const Vue2SmoothScroll: PluginFunction<SmoothScrollOptions>;
+
+export default Vue2SmoothScroll;
